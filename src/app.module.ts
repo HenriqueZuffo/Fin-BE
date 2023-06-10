c1cdcfa592393caa1b5f4aac359a46762ed6d8b1
Henrique Zuffo <henriquezuffo@gmail.com>
2023-06-10 18:45:57 -0300

    Add controller and service account

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [WalletModule],
+  imports: [WalletModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
