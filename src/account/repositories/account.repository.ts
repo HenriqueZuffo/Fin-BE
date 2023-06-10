665ce6ba9314214432a4eae691b81120259cdd47
Henrique Zuffo <henriquezuffo@gmail.com>
2023-06-10 20:29:19 -0300

    a

---
+++ src/account/repositories/account.repository.ts
@@ -0,0 +1,9 @@
+import { CreateAccountDto } from '../dto/create-account.dto';
+import { UpdateAccountDto } from "../dto/update-account.dto";
+
+export interface AccountRepository {
+  create(account: CreateAccountDto): Promise<string>;
+  update(account: UpdateAccountDto): Promise<void>;
+  delete(id: number): Promise<void>;
+  login(id: number): Promise<boolean>;
+}
