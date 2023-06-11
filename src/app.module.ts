133dfae5c7b00ac7b3a9305de483fb6c205be501
Henrique Zuffo <henriquezuffo@gmail.com>
2023-06-11 11:10:18 -0300

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 import { AccountModule } from './account/account.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WalletModule, AccountModule],
+  imports: [WalletModule, AccountModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
