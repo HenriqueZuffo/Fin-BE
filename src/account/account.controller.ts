964aef354bc32fcac96367bc367d489923384ebe
Henrique Zuffo <henriquezuffo@gmail.com>
2023-06-13 20:42:20 -0300

    eslint fix

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -2,14 +2,12 @@ import {
   Body,
   Controller,
   HttpCode,
-  HttpException,
   HttpStatus,
   Post,
 } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
-import { Prisma } from '@prisma/client';
-import { AppUtils } from "../app.utils";
+import { AppUtils } from '../app.utils';
 
 @Controller('account')
 export class AccountController {
--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { AccountRepository } from './repositories/account.repository';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AccountService {
